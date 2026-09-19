05c18dd3284c29b6d36934ec196d6320b033b16d
Pxy0903 <2311824629@qq.com>
2026-09-19 11:22:08 +0800

    第三次提交，纯代码实现多语言功能，删除了layout布局文件。第一次作业完成。

--- app/src/main/java/cn/edu/sicnu/cs/stu/pengxiangyuan/first/MyActivity.kt
+++ app/src/main/java/cn/edu/sicnu/cs/stu/pengxiangyuan/first/MyActivity.kt
@@ -1,6 +1,7 @@
 package cn.edu.sicnu.cs.stu.pengxiangyuan.first
 
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.LinearLayout
@@ -9,12 +10,26 @@ import android.widget.TextView
 class MyActivity : AppCompatActivity () {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.button)
+
+        val linearLayout = LinearLayout(this)
+        linearLayout.orientation = LinearLayout.VERTICAL
+
+        val tv_hello = TextView(this)
+        tv_hello.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT)
+        tv_hello.textSize = 30F
+        tv_hello.text = getString(R.string.hello_world)
+        linearLayout.addView(tv_hello)
+
+        val button = Button(this)
+        button.width = ViewGroup.LayoutParams.MATCH_PARENT
+        button.height = ViewGroup.LayoutParams.WRAP_CONTENT
+        button.text = getString(R.string.btn_click_me)
+        linearLayout.addView(button)
+
+        setContentView(linearLayout)
         button.setOnClickListener {
             // TODO: 点击事件逻辑
-            val textView = findViewById<TextView>(R.id.tv_hello)
-            textView.text = resources.getString(R.string.clicked_text)
+            tv_hello.text = resources.getString(R.string.clicked_text)
         }
     }
 }
